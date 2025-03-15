e7c4b6dc2f340bd6a12468f8c6a8672058b17218
prestonakagi <preston.akagi@gmail.com>
2025-03-15 17:46:58 -0600

    Finished temples picture album

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,25 @@
+// footer year and datetime last modified
+
+// select the DOM element for year output
+const year = document.getElementById("currentyear");
+
+// use the date object
+const today = new Date();
+
+const currentYear = today.getFullYear();
+
+// change text displayed on rendered web page
+year.innerText = currentYear
+
+
+// select the DOM element for last modified output
+const lastMod = document.getElementById("lastModified");
+
+// use lastModified property of document
+// default format is mm/dd/yyyy hh:mm:ss
+lastMod.innerText = document.lastModified;
+
+
 // responsive hamburger effect on nav menu.
 
 // hamburger button only show in mobile (small) view.
@@ -8,6 +30,6 @@ const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-navigation.classList.toggle('show');
-hamButton.classList.toggle('show');
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('show');
 });

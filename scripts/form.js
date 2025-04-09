5260f9142ef027a7f584748b636ab45a117768c6
prestonakagi <preston.akagi@gmail.com>
2025-04-09 02:01:37 -0600

    form project almost done, but just need to do localStorage part

---
+++ scripts/form.js
@@ -0,0 +1,61 @@
+// footer year and datetime last modified
+
+// select the DOM element for year output
+const year = document.getElementById("currentyear");
+
+// use the date object
+const today = new Date();
+
+const currentYear = today.getFullYear();
+
+// change text displayed on rendered web page
+year.innerText = currentYear
+
+
+// select the DOM element for last modified output
+const lastMod = document.getElementById("lastModified");
+
+// use lastModified property of document
+// default format is mm/dd/yyyy hh:mm:ss
+lastMod.innerText = document.lastModified;
+
+
+// to fill select element's drop down list options
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+// create option elements and append to class="select"
+
+products.forEach(function(product) {
+    let select = document.querySelector(".select");
+    let option = document.createElement('option');
+    option.value = product.id; // .value is the value attribute.
+    option.text = product.name; // .text is what is seen.
+    select.appendChild(option);
+});

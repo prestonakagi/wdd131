d820c4d582a34eee2cb77beec738f3739de0b826
prestonakagi <preston.akagi@gmail.com>
2025-04-10 02:46:22 -0600

    initialized final project index html, css, js

---
+++ project/scripts/bioinfo-protein.js
@@ -0,0 +1,21 @@
+// footer year and datetime last modified
+
+// select the DOM element for year output
+const year = document.getElementById("currentyear");
+
+// use the date object
+const today = new Date();
+
+const currentYear = today.getFullYear();
+
+// change text displayed on rendered web page
+year.innerText = currentYear
+
+
+// select the DOM element for last modified output
+const lastMod = document.getElementById("lastModified");
+
+// use lastModified property of document
+// default format is mm/dd/yyyy hh:mm:ss
+lastMod.innerText = document.lastModified;
+

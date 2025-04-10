50e759e18d2eec13755bcbe1bc1fe8a27e36f336
prestonakagi <preston.akagi@gmail.com>
2025-04-10 01:22:45 -0600

    done with form project. Couldn't get anything stored in localStorage.

--- scripts/form.js
+++ scripts/form.js
@@ -59,3 +59,33 @@ products.forEach(function(product) {
     option.text = product.name; // .text is what is seen.
     select.appendChild(option);
 });
+
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", () => {
+
+    // 1️⃣ Initialize display element variable
+    const visitsDisplay = document.querySelector(".sent-reviews");
+      
+    // 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+    let numVisits = parseInt(localStorage.getItem("numVisits-ls")) || 0;
+    // pageLoadCount = pageLoadCount ? parseInt(pageLoadCount) : 0;
+      
+    // 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+    if (numVisits !== 0) {
+     visitsDisplay.textContent = numVisits;
+    } else {
+     visitsDisplay.textContent = `Hi! This is your first visit.`;
+    }
+    
+    // 4️⃣ increment the number of visits by one.
+    numVisits++;
+    
+    // 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+    localStorage.setItem("numVisits-ls", numVisits);
+    
+    // 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
+
+    // window.location.href = "review.html";
+  });

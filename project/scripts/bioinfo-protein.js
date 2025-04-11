9a252f95e8e2938deb34e59168c94dd9a2f8e63f
prestonakagi <preston.akagi@gmail.com>
2025-04-11 01:17:52 -0600

    made header, footer, and h1 headings for all pages. Established the other 2 pages than index

--- project/scripts/bioinfo-protein.js
+++ project/scripts/bioinfo-protein.js
@@ -19,3 +19,17 @@ const lastMod = document.getElementById("lastModified");
 // default format is mm/dd/yyyy hh:mm:ss
 lastMod.innerText = document.lastModified;
 
+
+// responsive hamburger effect on nav menu.
+
+// hamburger button only show in mobile (small) view.
+// clicking hamburger button toggles nav menu items from viewable to not viewable.
+// use a symbol, like "X", to close hamburger menu.
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
